b587dd26e28eacb7af9d78566f042d563c78f75c
Hoàn Nguyễn <softteam2021@gmail.com>
2021-11-24 01:24:38 +0700

    create cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React, { Suspense, lazy } from 'react';
 import { Header, Nav } from './components';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 
 const HomePage = lazy(() => import('./pages/Home'))
 const Detail = lazy(() => import('./pages/DetailProduct'))
+const Cart = lazy(() => import('./pages/Cart'))
 function App() {
   return (
     <Suspense fallback={<h1>Loading</h1>}>
       <Header />
       <Nav />
       <Switch>
-        <Route path='/' component={HomePage} />
+        {/* <Route path='/' component={HomePage} /> */}
         {/*<Route path='/' component={Detail}/>*/}
-      </Switch>
+        <Route path='/' component={Cart} />
+     </Switch>
     </Suspense>
   );
 }

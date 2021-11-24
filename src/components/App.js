d2c3b15ac5d392573209d6fdebb7f9e7e48cecfd
danhpq176 <danhpq176@github.com>
2021-11-24 21:19:33 +0700

    add page checkout

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,15 @@ import { Switch, Route } from 'react-router-dom'
 
 const HomePage = lazy(() => import('./pages/Home'))
 const Detail = lazy(() => import('./pages/DetailProduct'))
+const Checkout = lazy(() => import('./pages/Checkout'))
 function App() {
   return (
     <Suspense fallback={<h1>Loading</h1>}>
       <Header />
       <Nav />
       <Switch>
-        <Route path='/' component={HomePage} />
-        {/*<Route path='/' component={Detail}/>*/}
+        {/* <Route path='/' component={HomePage} /> */}
+        <Route path='/' component={Checkout}/>
       </Switch>
     </Suspense>
   );

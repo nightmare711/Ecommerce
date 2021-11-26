838231083d7c546536e9827079dd736adb4e6603
Hoàn Nguyễn <softteam2021@gmail.com>
2021-11-27 00:44:59 +0700

    finish blog

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,16 @@ import { Switch, Route } from 'react-router-dom'
 
 const HomePage = lazy(() => import('./pages/Home'))
 const Detail = lazy(() => import('./pages/DetailProduct'))
+const Blog = lazy(() => import('./pages/Blog'))
 function App() {
   return (
     <Suspense fallback={<h1>Loading</h1>}>
       <Header />
       <Nav />
       <Switch>
-        <Route path='/' component={HomePage} />
+        {/* <Route path='/' component={HomePage} /> */}
         {/*<Route path='/' component={Detail}/>*/}
+        <Route path='/' component={Blog}/>
       </Switch>
     </Suspense>
   );
